76439671d374a124d95b7354dbfc44c3e2c75816
oleksandr-chekalenko <Oleksandr.Chekalenko.-ND@disneystreaming.com>
2022-11-21 13:53:18 +0200

    Project init

---
+++ src/test/java/com/chelex/SpringDataMongoDb/SpringDataMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chelex.SpringDataMongoDb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataMongoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
